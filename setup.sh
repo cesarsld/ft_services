f796bfea9ffe08d392cde64204c1b21bab71adaf
cjaimes@student.42.fr <cjaimes@student.42.fr>
2020-01-28 18:49:39 +0100

    forgot to add

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 brew install minikub
-minikube start --vm-driver=virtualbox
+minikube start --vm-driver=virtualbox --extra-config=apiserver.service-node-port-range=1-35000
+export MINIKUBE_IP=$(minikube ip)
+echo "Minikube IP is $MINIKUBE_IP"
 minikube ssh
 # minikube dashboard # to check state etc
+kubectl apply -f wordpress.yaml
