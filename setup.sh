545f70fb8eee81554499861ae2347e375e59559c
cjaimes@student.42.fr <cjaimes@student.42.fr>
2020-02-11 17:23:03 +0100

    working project

--- setup.sh
+++ setup.sh
@@ -1,17 +1,3 @@
-
-# Build function
-
-function apply_yaml()
-{
-	kubectl apply -f srcs/$@.yaml > /dev/null
-	printf "Deploying $@...\n"
-	sleep 2;
-	while [[ $(kubectl get pods -l app=$@ -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do
-		sleep 1;
-	done
-	printf "$@ deployed!\n"
-}
-
 SERVICES="mysql phpmyadmin nginx wordpress ftps influxdb grafana telegraf"
 
 if [[ $1 = 'clean' ]]
@@ -19,9 +5,8 @@ then
 	printf "Cleaning all services...\n"
 	for SERVICE in $SERVICES
 	do
-		kubectl delete -f srcs/yaml/$SERVICE.yaml > /dev/null
+		kubectl delete -f srcs/yaml/$SERVICE.yaml
 	done
-	kubectl delete -f srcs/ingress.yaml > /dev/null
 	printf "Clean complete !\n"
 	exit
 fi
@@ -48,8 +33,6 @@ cp srcs/pods/mysql/wordpress.sql srcs/pods/mysql/wordpress-tmp.sql
 sed -i '' "s/MINIKUBE_IP/$MINIKUBE_IP/g" srcs/pods/mysql/wordpress-tmp.sql
 cp srcs/pods/wordpress/wp-config.php srcs/pods/wordpress/wp-config-tmp.php
 sed -i '' "s/MINIKUBE_IP/$MINIKUBE_IP/g" srcs/pods/wordpress/wp-config-tmp.php
-cp srcs/pods/ftps/vsftpd.conf srcs/pods/ftps/vsftpd-tmp.conf
-sed -i '' "s/MINIKUBE_IP/$MINIKUBE_IP/g" srcs/pods/ftps/vsftpd-tmp.conf
 cp srcs/pods/ftps/start.sh srcs/pods/ftps/start-tmp.sh
 sed -i '' "s/MINIKUBE_IP/$MINIKUBE_IP/g" srcs/pods/ftps/start-tmp.sh
 cp srcs/yaml/telegraf_model.yaml srcs/yaml/telegraf.yaml
@@ -69,11 +52,6 @@ docker build -t services/ftps srcs/pods/ftps
 docker build -t services/grafana srcs/pods/grafana
 docker build -t services/telegraf srcs/pods/telegraf
 
-# for SERVICE in $SERVICE_LIST
-# do
-# 	apply_yaml $SERVICE
-# done
-
 kubectl apply -f srcs/yaml/grafana.yaml
 kubectl apply -f srcs/yaml/influxdb.yaml
 kubectl apply -f srcs/yaml/mysql.yaml
@@ -83,13 +61,7 @@ kubectl apply -f srcs/yaml/wordpress.yaml
 kubectl apply -f srcs/yaml/ftps.yaml
 kubectl apply -f srcs/yaml/telegraf.yaml
 
-# kubectl apply -f srcs/ingress.yaml > /dev/null
 
-# Import Wordpress database
-# kubectl exec -i $(kubectl get pods | grep mysql | cut -d" " -f1) -- mysql -u root -e 'CREATE DATABASE wordpress;'
-# kubectl exec -i $(kubectl get pods | grep mysql | cut -d" " -f1) -- mysql wordpress -u root < srcs/wordpress/files/wordpress-tmp.sql
-
-# rm -rf srcs/ftps/scripts/start-tmp.sh
 rm -rf srcs/pods/mysql/wordpress-tmp.sql
 rm -rf srcs/pods/wordpress/wp-config-tmp.sql
 rm -rf srcs/pods/ftps/vsftpd-tmp.conf
@@ -99,5 +71,11 @@ rm -rf srcs/yaml/telegraf.yaml
 server_ip=`minikube ip`
 echo -ne "\033[1;33m+>\033[0;33m IP : $server_ip \n"
 
+### Launch Dashboard
+# minikube dashboard
+
 ### Test SSH
 # ssh admin@$(minikube ip) -p 6666
+
+### Crash Container
+# kubectl exec -it $(kubectl get pods | grep mysql | cut -d" " -f1) -- /bin/sh -c "kill 1"
